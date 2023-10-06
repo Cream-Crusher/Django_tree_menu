506b57a2e6cd9c666543397649744ce413560ef0
Cream-Crusher <icecreamcrusher21@gmail.com>
2023-10-06 16:46:53 +0400

    customized admin panel

--- application/admin.py
+++ application/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from application.models import TreeNode
 
 
+class ChildInline(admin.TabularInline):
+    model = TreeNode
+    extra = 1
+
+
 @admin.register(TreeNode)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class TreeNodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_name', 'parent')
+    list_filter = ('parent',)
+    search_fields = ('display_name',)
+    inlines = [ChildInline]
