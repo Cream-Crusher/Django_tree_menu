4ba72f224e004e7054d7555e281c9c3f93266143
Cream-Crusher <icecreamcrusher21@gmail.com>
2023-10-06 15:57:44 +0400

    add model(Node)

--- application/models.py
+++ application/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Node(models.Model):
+    display_name = models.CharField(
+        max_length=50,
+        db_index=True,
+        null=False)
+
+    parent = models.OneToOneField(
+        'self',
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='child'
+    )
+
+    children = models.ManyToManyField(
+        'self',
+        blank=True,
+        related_name='parents'
+    )
+
+    def __str__(self):
+        return self.display_name
